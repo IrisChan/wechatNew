357256922e033eb7b6532642a0d0910184a4f2dd
IrisPolaris <chen18@ualberta.ca>
2017-06-13 21:58:12 -0400

    Add login page: part 1 in containers

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,5 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import * as LoginActions from '../actions';
 
-const App = () =>
-	<button className="btn btn-primary">Press me!</button>;
-export default App;
+const Login = ({credentials, actions}) => (
+	<div>
+		<Login credentials={credentials} actions={actions} />
+	</div>
+);
+
+Login.propTypes = {
+	actions: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+	credentials: state.credentials
+});
+
+const mapDispatchToProps = dispatch => ({
+	actions: bindActionCreators(LoginActions, dispatch)
+});
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(Login);
